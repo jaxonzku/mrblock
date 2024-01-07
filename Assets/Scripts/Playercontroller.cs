e1ae4a4b75ed3ee42c8ade3876e8f0567c0f2b7f
Jackson James <jackson.james@converj.com>
2024-01-08 00:32:01 +0530

    speed and break

--- Assets/Scripts/Playercontroller.cs
+++ Assets/Scripts/Playercontroller.cs
@@ -13,41 +13,56 @@ public class Playercontroller : MonoBehaviour
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-       
+        HandleMovementInput();
 
-        if (Input.GetAxis("Horizontal") > 0 && Input.GetAxis("Jump")==0)
-        {
+        /*
+                if (Input.GetAxis("Horizontal") > 0 && Input.GetAxis("Jump")==0)
+                {
 
-            rigidbody2D.velocity = new Vector2(speed, 0f);
+                    rigidbody2D.velocity = new Vector2(speed, 0f);
 
-        }
-        else if (Input.GetAxis("Horizontal") < 0 && Input.GetAxis("Jump") == 0)
-        {
-            rigidbody2D.velocity = new Vector2(-speed, 0f);
+                }
+                else if (Input.GetAxis("Horizontal") < 0 && Input.GetAxis("Jump") == 0)
+                {
+                    rigidbody2D.velocity = new Vector2(-speed, 0f);
 
-        }
-        else if(Input.GetAxis("Vertical")> 0 && Input.GetAxis("Jump") == 0)
-        {
-            rigidbody2D.velocity = new Vector2( 0f, speed);
+                }
+                else if(Input.GetAxis("Vertical")> 0 && Input.GetAxis("Jump") == 0)
+                {
+                    rigidbody2D.velocity = new Vector2( 0f, speed);
 
-        }
-        else if(Input.GetAxis("Vertical") < 0 && Input.GetAxis("Jump") == 0)
-        {
-            rigidbody2D.velocity = new Vector2(0f ,- speed);
-        }
-        else if(Input.GetAxis("Horizontal")==0 & Input.GetAxis("Horizontal")==0)
-        {
+                }
+                else if(Input.GetAxis("Vertical") < 0 && Input.GetAxis("Jump") == 0)
+                {
+                    rigidbody2D.velocity = new Vector2(0f ,- speed);
+                }
+                else if(Input.GetAxis("Horizontal")==0 & Input.GetAxis("Horizontal")==0)
+                {
 
-            rigidbody2D.velocity = new Vector2(0f, 0f);
-        }
-      
+                    rigidbody2D.velocity = new Vector2(0f, 0f);
+                }
+
+                */
+     
 
     }
 
 
+    void HandleMovementInput()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector2 inputVector = new Vector2(horizontalInput, verticalInput).normalized;
+
+        if (inputVector != Vector2.zero && Input.GetAxis("Jump") == 0)
+        {
+            transform.Translate(inputVector * speed * Time.deltaTime);
+        }
+      
+    }
     private void OnTriggerEnter2D(Collider2D other)
     {
         if(other.tag=="door")
