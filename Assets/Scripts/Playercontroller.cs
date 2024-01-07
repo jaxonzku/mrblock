a507bed80f8086e46271c5ff2f346789e073bdd9
Jackson James <jackson.james@converj.com>
2024-01-08 00:20:02 +0530

    added break

--- Assets/Scripts/Playercontroller.cs
+++ Assets/Scripts/Playercontroller.cs
@@ -16,23 +16,25 @@ public class Playercontroller : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+       
 
-        if (Input.GetAxis("Horizontal") > 0)
+        if (Input.GetAxis("Horizontal") > 0 && Input.GetAxis("Jump")==0)
         {
+
             rigidbody2D.velocity = new Vector2(speed, 0f);
 
         }
-        else if (Input.GetAxis("Horizontal") < 0)
+        else if (Input.GetAxis("Horizontal") < 0 && Input.GetAxis("Jump") == 0)
         {
             rigidbody2D.velocity = new Vector2(-speed, 0f);
 
         }
-        else if(Input.GetAxis("Vertical")> 0)
+        else if(Input.GetAxis("Vertical")> 0 && Input.GetAxis("Jump") == 0)
         {
             rigidbody2D.velocity = new Vector2( 0f, speed);
 
         }
-        else if(Input.GetAxis("Vertical") < 0)
+        else if(Input.GetAxis("Vertical") < 0 && Input.GetAxis("Jump") == 0)
         {
             rigidbody2D.velocity = new Vector2(0f ,- speed);
         }
@@ -40,10 +42,11 @@ public class Playercontroller : MonoBehaviour
         {
 
             rigidbody2D.velocity = new Vector2(0f, 0f);
-
         }
+      
 
     }
+
 
     private void OnTriggerEnter2D(Collider2D other)
     {
